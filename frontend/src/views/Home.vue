c5fab91622a408c3ed1fb2e6bec1c7ff129c575f
Matheus Melo Antiquera <matheus.melo.a@hotmail.com>
2019-04-27 20:51:41 -0300

    First commit

---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="home">
+    <b-row>
+      <b-col v-for="column in columns" :key="column.name" class="no-padding">
+         <b-list-group-item>{{ column.name }}</b-list-group-item>
+        <draggable :list="column.cards" group="cards" tag="b-list-group">
+          <b-list-group-item v-for="card in column.cards" :key="card.id">{{ card.name }}</b-list-group-item>
+        </draggable>
+        <b-list-group-item slot="footer">
+            <b-button block variant="primary">New</b-button>
+        </b-list-group-item>
+      </b-col>
+    </b-row>
+  </div>
+</template>
+
+<script>
+import draggable from "vuedraggable";
+
+export default {
+  components: {
+    draggable
+  },
+  data() {
+    return {
+      columns: [
+        {
+          name: "ToDo",
+          cards: [
+            { id: 1, name: "Lavar roupa" },
+            { id: 2, name: "Ir ao mercado" },
+            { id: 3, name: "Tomar banho" }
+          ]
+        },
+        {
+          name: "Done",
+          cards: [{ id: 5, name: "Lavar roupa" }]
+        }
+      ]
+    };
+  }
+};
+</script>
+<style>
+.no-padding{
+  padding: 0;
+}
+</style>
